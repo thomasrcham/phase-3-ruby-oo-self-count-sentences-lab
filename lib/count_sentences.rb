98a97059812b6cc7d939c77129b9a74e8c5d413d
thomasrcham <guseldorph@gmail.com>
2022-10-12 09:58:40 -0500

    commit tree d65f85abd72e5a916144008286af7027331a2d13

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
-require 'pry'
+require "pry"
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    splitSentences = self.split(/[.?!]+/)
+    splitSentences.count
   end
 end
